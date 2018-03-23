5157cdd3437700659d050f48af65a282228d29b0
Paulowskie <allpau23@gmail.com>
2018-03-23 08:25:56 +0800

    a

---
+++ WebAPI/DTO/ContactDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ContactDTO
+    {
+        public int id { get; set; }
+        public string accountNumber { get; set; }
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+        public string contactNo { get; set; }
+        public bool isMale { get; set; }
+        public decimal balance { get; set; }
+
+    }
+}
